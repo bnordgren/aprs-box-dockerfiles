a71a70bc1609435f999e938b870b6d0577b79677
Bryce <bnordgren@gmail.com>
2017-04-20 15:24:04 +0000

    automatically fill in callsign in aprsd config files.

--- polaric-server/polaric-startup.sh
+++ polaric-server/polaric-startup.sh
@@ -2,5 +2,6 @@
 
 rm -f /var/run/polaric-aprsd.pid
 sed -i "s/%MY_IP%/$MY_IP/g" /etc/polaric-webapp/mapconfig.js
+sed -i "s/%MYCALL%/$CALLSIGN/g" /etc/polaric-aprsd/aprsd-server.ini
 /etc/init.d/polaric-aprsd restart
 apachectl -D FOREGROUND
